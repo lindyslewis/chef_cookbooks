2784cf4f77dc1a98ebcfc2f2839de5b381037f60
Lindy Lewis <lindyslewis@gmail.com>
2018-02-13 21:36:53 -0600

    apache cookbook

---
+++ apache/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+package 'httpd'
+
+template '/var/www/html/index.html' do 
+  mode '0755'
+  owner 'apache'
+  group 'apache'
+  source 'index.html.erb'
+end
+
+service 'httpd' do 
+  action [:enable :start]
+  retries 3
+end
